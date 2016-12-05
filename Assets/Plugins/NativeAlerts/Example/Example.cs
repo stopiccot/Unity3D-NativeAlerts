cfcd0d08eb563bc8ccbb5adefd2801195fc525c1
stopiccot <alexey.petruchik@gmail.com>
2016-12-05 20:27:45 +0300

    Initial commit

---
+++ Assets/Plugins/NativeAlerts/Example/Example.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+	public void OnButtonClick() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorUtility.DisplayDialog("title", "message", "ok");
+		#endif
+	}
+}
